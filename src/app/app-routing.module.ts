c5e10625d8477a3b70d578e06e395f55ab86d2c5
eeshwersaicharan <seeshwar@innominds.com>
2021-11-28 21:11:19 +0530

    components updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -7,7 +8,8 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path:'',component:LandingComponent},
   {path:'register',component:RegisterComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'forgot',component:ForgetpasswordComponent}
 ];
 
 @NgModule({
